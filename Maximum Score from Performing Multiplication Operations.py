a2dacba4fa8820932c23ab8c1e13efc1e01566f7
SabareesanArjunasamy <sabareesan833@gmail.com>
2022-09-17 01:55:32 +0530

    DP - Memoization

---
+++ Maximum Score from Performing Multiplication Operations.py
@@ -0,0 +1,18 @@
+def maximumScore(nums, multipliers) -> int:
+        n = len(nums)
+        m = len(multipliers)
+        
+        dp = [[0]*(m+1) for i in range(m+1)]
+        for j in range(m-1,-1,-1):
+            for low in range(j,-1,-1):
+                first = nums [low]*multipliers[j]+dp[j+1][low+1]
+                last = nums[n-1-(j-low)]*multipliers[j]+dp[j+1][low]
+                dp[j][low] = max(first, last)
+        return dp[0][0]
+    
+nums,multipliers  = [1,2,3],[3,2,1]
+print(maximumScore(nums,multipliers))
+# expected Output: 14
+nums,multipliers  = [-5,-3,-3,-2,7,1],[-10,-5,3,4,6]
+print(maximumScore(nums,multipliers))
+# expected Output: 102
