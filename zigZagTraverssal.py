3f458c53b26d6465b59e20f58b79ff061305d853
Sabareesan Arjunasamy <54575731+SabareesanArjunasamy@users.noreply.github.com>
2022-10-16 12:20:02 +0530

    Update Zigzag Traversal

--- zigZagTraverssal.py
+++ zigZagTraverssal.py
@@ -32,12 +32,12 @@ def zigzagTraverse(array):
             res.append(array[i][j])
     return res
 
-array = [
+matrix = [
   [1, 3, 4, 10],
   [2, 5, 9, 11],
   [6, 8, 12, 15],
   [7, 13, 14, 16]
 ]
 
-x = zigzagTraverse(array)
+x = zigzagTraverse(matrix)
 print(x)
