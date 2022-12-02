a61a8baab266bfda28895c3ea4e65f6e5d34c297
SabareesanArjunasamy <sabareesan833@gmail.com>
2022-12-02 22:27:41 +0530

    2 Dec 2022

--- coseStrings.py
+++ coseStrings.py
@@ -6,7 +6,6 @@ def closeStrings(word1: str, word2: str) -> bool:
     c2 = collections.Counter(word2)
     set1 = set(word1)
     set2 = set(word2)
-
     s1 = sorted(c1.values())
     s2 = sorted(c2.values())
     return s1 == s2 and set1 == set2
