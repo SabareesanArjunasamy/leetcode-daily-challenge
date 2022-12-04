7962e99b64c1099d96f56cffec192ac8361904a4
SabareesanArjunasamy <sabareesan833@gmail.com>
2022-12-04 21:25:36 +0530

    4 Dec 2022

---
+++ minAvgDiff.py
@@ -0,0 +1,35 @@
+def minimumAverageDifference(self, nums: List[int]) -> int:
+    l = [-1]*len(nums)
+    r = [-1]*len(nums)
+
+    def rightmean(i):
+        if i == 0:
+            r[i] = sum(nums[i+1:])
+        elif i == len(nums)-1:
+            r[i] = 0
+        else:
+            r[i] = r[i-1] - nums[i]
+
+    def leftmean(i):
+        if i == 0:
+            l[i] = nums[i]
+        else:
+            l[i] = l[i-1] + nums[i]
+
+    def average(i):
+        la = l[i] // (i+1)
+        ra = r[i] // (len(nums)-i-1) if (len(nums)-i-1) != 0 else 0
+        avg = abs(la - ra)
+        return avg
+
+    for i in range(len(nums)):
+        rightmean(i)
+        leftmean(i)
+    res = 0
+    minavg = math.inf
+    for i in range(len(nums)):
+        avg = average(i)
+        if avg < minavg:
+            minavg = avg
+            res = i
+    return res
