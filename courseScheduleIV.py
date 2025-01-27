0d6fd6e05bbbe1b7b359c15b7caba766cb4ca265
SabareesanArjunasamy <sabareesan833@gmail.com>
2025-01-27 23:55:29 +0530

    There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course ai first if you want to take course bi.
    For example, the pair [0, 1] indicates that you have to take course 0 before you can take course 1.
    Prerequisites can also be indirect. If course a is a prerequisite of course b, and course b is a prerequisite of course c, then course a is a prerequisite of course c.
    
    You are also given an array queries where queries[j] = [uj, vj]. For the jth query, you should answer whether course uj is a prerequisite of course vj or not.
    
    Return a boolean array answer, where answer[j] is the answer to the jth query.

---
+++ courseScheduleIV.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkIfPrerequisite(
+        self,
+        numCourses: int,
+        prerequisites: List[List[int]],
+        queries: List[List[int]],
+    ) -> List[bool]:
+        isPrerequisite = [[False] * numCourses for _ in range(numCourses)]
+
+        for edge in prerequisites:
+            isPrerequisite[edge[0]][edge[1]] = True
+
+        for intermediate in range(numCourses):
+            for src in range(numCourses):
+                for target in range(numCourses):
+                    # If there is a path src -> intermediate and intermediate -> target, then src -> target exists as well
+                    isPrerequisite[src][target] = isPrerequisite[src][
+                        target
+                    ] or (
+                        isPrerequisite[src][intermediate]
+                        and isPrerequisite[intermediate][target]
+                    )
+
+        answer = []
+        for query in queries:
+            answer.append(isPrerequisite[query[0]][query[1]])
+
+        return answer
